90d9a961f129ff33e57053dfd55e23d7b274bd5d
waleaudumbari <audumbariw@gmail.com>
2024-10-10 13:33:00 +0530

    Univercity system

---
+++ src/university/management/system/About.java
@@ -0,0 +1,49 @@
+package university.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class About extends JFrame
+{   
+
+    About() 
+    {
+        setSize(700, 500);
+        setLocation(400, 150);
+        getContentPane().setBackground(Color.WHITE);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/about.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 0, 300, 200);
+        add(image);
+        
+        JLabel heading = new JLabel("<html>University<br/>Management System</html>");
+        heading.setBounds(70, 20, 300, 130);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(heading);
+        
+        JLabel name = new JLabel("Developed By: Wale Audumbari");
+        name.setBounds(70, 220, 550, 40);
+        name.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(name);
+        
+        JLabel rollno = new JLabel("Seat number: 5731");
+        rollno.setBounds(70, 280, 550, 40);
+        rollno.setFont(new Font("Tahoma", Font.PLAIN, 30));
+        add(rollno);
+        
+        JLabel contact = new JLabel("Contact: waleaudumbari2001@@gmail.com and 8999698542");
+        contact.setBounds(70, 340, 550, 40);
+        contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        add(contact);
+        
+        setLayout(null);
+        
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new About();
+    }
